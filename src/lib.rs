d6faa29ab34f991b18c1fabfaf22a6e81720cdf4
RunDevelopment <mitchi5000.ms@googlemail.com>
2025-07-22 11:54:15 +0200

    Docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! A module for modeling the behavior of std functions using integer
+//! arithmetic.
+//!
+//! Currently, only integer intervals are supported, but floating point
+//! intervals can be added later.
+
 mod arithmetic;
 mod bits;
 mod iinterval;
